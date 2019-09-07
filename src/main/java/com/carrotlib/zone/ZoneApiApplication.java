0ecfd9f0aca42d5b86349890a9854c6d70d1c8ca
carrot <342408524@qq.com>
2019-09-08 01:18:45 +0800

    init project

---
+++ src/main/java/com/carrotlib/zone/ZoneApiApplication.java
@@ -0,0 +1,13 @@
+package com.carrotlib.zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoneApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoneApiApplication.class, args);
+    }
+
+}
